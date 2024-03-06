f3c151f1543db5e4eaa57527cdb77e238749c4ab
ShaunakKDatar <shaunakkdatar@gmail.com>
2024-03-07 03:08:36 +0530

    Updated Auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 
 module.exports = function (req, res, next) {
-  const token = req.header("x-auth-token");
+  const token = req.header("X-auth-token");
   if (!token) return res.status(401).send("Access Denied. No token Provided.");
 
   try {
