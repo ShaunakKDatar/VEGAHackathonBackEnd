e4ee8d4530919a750e7ab8ddb35c86cdc408e43d
Soham Sunil Margaj <sohammargaj55555@gmail.com>
2024-03-06 14:24:46 +0530

    change name of user model and user routes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const jwt =require("jsonwebtoken");
-const User =require('../models2/user');
+const User =require('../models/user');
 
 // Login route
 router.post('/login', async (req, res) => {
